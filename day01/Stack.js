a492a0bec0043b957f52f0ded23c50f7faa3a7b0
xiaoznz <779740337@qq.com>
2021-12-30 09:50:55 +0800

    提ä用html展示算法

--- day01/Stack.js
+++ day01/Stack.js
@@ -1,14 +1,12 @@
 /**
  * @FileName: Stack.ts
  * @Description: 栈结构的封装
- * @author 毛毛
+ * @author xiaoznz
  * @date 2021-07-14 16:32
  */
 
 //栈类
-class Stack<T> {
-    // 栈中存元素的数组
-    private readonly items: Array<T>;
+class Stack {
 
     constructor() {
         this.items = []
@@ -18,7 +16,7 @@ class Stack<T> {
      * 压入元素到栈顶的方法
      * @param ele
      */
-    public push(ele: T): void {
+    push(ele) {
         this.items.push(ele);
     }
 
@@ -26,27 +24,27 @@ class Stack<T> {
      * 弹出栈顶元素
      * @return: 弹出的栈顶元素
      */
-    public pop(): T {
-        return this.items.pop() as T;
+    pop() {
+        return this.items.pop()
     }
 
     // 查看栈顶元素
-    public peek(): T {
+    peek() {
         return this.items[this.items.length - 1];
     }
 
     // 栈是否为空
-    isEmpty(): boolean {
+    isEmpty() {
         return this.items.length === 0;
     }
 
 //  返回栈结构里面的元素个数
-    size(): number {
+    size() {
         return this.items.length;
     }
 
 //  打印栈结构里面的所有元素，以字符串的形式打印
-    public toString(): string {
+    toString() {
         return this.items.join(',');
     }
 }
